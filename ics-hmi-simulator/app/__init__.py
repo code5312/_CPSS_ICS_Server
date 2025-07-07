e304255c4a3177d0f2e794e3824cfed81e8cecb4
sooooap <162416952+sooooap@users.noreply.github.com>
2025-07-07 21:50:13 +0900

    Update __init__.py

--- ics-hmi-simulator/app/__init__.py
+++ ics-hmi-simulator/app/__init__.py
@@ -8,6 +8,7 @@ def create_app():
     app.secret_key = 'ics-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_COOKIE_HTTPONLY'] = False
 
     db.init_app(app)
 
