51b243a8ddc84792cf97f2581c583060b733e0ef
sooooap <162416952+sooooap@users.noreply.github.com>
2025-07-15 04:36:43 +0900

    Update routes.py

--- ics-hmi-simulator/app/routes.py
+++ ics-hmi-simulator/app/routes.py
@@ -318,6 +318,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from markupsafe import Markup
 
 @main.before_request
